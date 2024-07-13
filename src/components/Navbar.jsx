256234d84898baae8a7f0ef2467d6a2340d9db67
Ian Zevallos <ian.zevallos.m@gmail.com>
2024-07-13 19:45:01 +0200

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="py-5">
+      <div className="container">hellow</div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <section className="navbar">
+      <Navbar />
+    </section>
+  );
+};
+
+export default Home;
